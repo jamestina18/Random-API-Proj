6a5a70c9114f301c85d9ad7a81c4ce0de04a932a
Tina Flores <jamestina18@gmail.com>
2021-03-30 18:34:21 -0700

    Add files via upload

--- RandomUser_API.js
+++ RandomUser_API.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
           var number = $("input").val();
           $("input").val(" "); 
           console.log("top")
+          $("#buttonDEL").click(function(event) {
+               $("table").hide(2000)
+          }) 
+         
           // api call 
           $.ajax({
                url: `https://randomuser.me/api/?results=${number}`,
@@ -17,17 +21,18 @@ $(document).ready(function(){
                     var HtmlStr3 = ""
                     var HtmlStr4 = ""
 
-                    for(var i =0; i < data.lenght; i++) {
+                    for(var i =0; i < data.length; i++) {
                          console.log([i])
                          console.log("hellooooo")
                     HtmlStr1 += `<h2>${data[i].name.first} ${data[i].name.last}</h2>`
-                    HtmlStr2 += `<h4>${data[i].age}</h4>`
+                    HtmlStr2 += `<h4>${data[i].dob.age}</h4>`
                     HtmlStr3 += ` <h4>${data[i].email} </h4>`
                     HtmlStr4 += `<h4>
                     ${data[i].location.city}
                     ${data[i].location.street.number}
                     ${data[i].location.street.name}
                     </h4>`  
+
                    
                     }
                $('#name').append(HtmlStr1)
@@ -35,11 +40,13 @@ $(document).ready(function(){
                $('#email').append(HtmlStr3)
                $('#address').append(HtmlStr4)
 
-               $(document).on("click", "#buttonDel", function(){
-                  $(this).parent().remove()  
-               })
+               
                }
+              
+                
           })
+         
      })
      
 })
+
